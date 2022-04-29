9e8fd598b6a4d7ecaaba0f71ec2b51534f24ce24
Suray Sweed <suray.sweed@campus.technion.ac.il>
2022-04-29 21:45:13 +0300

    submmit

--- ex1/bp.cpp
+++ ex1/bp.cpp
@@ -2,23 +2,27 @@
 /* This file should hold your implementation of the predictor simulator */
 
 #include "bp_api.h"
+#include "branch_predictor.h"
 
-
+BranchPredictor bp(0, 0, 0, 0, 0, 0, 0);
 
 int BP_init(unsigned btbSize, unsigned historySize, unsigned tagSize, unsigned fsmState,
 			bool isGlobalHist, bool isGlobalTable, int Shared){
-	return -1;
+	bp = BranchPredictor(btbSize, historySize, tagSize, fsmState, isGlobalHist, isGlobalTable, Shared);
+	return 0;
 }
 
 bool BP_predict(uint32_t pc, uint32_t *dst){
-	return false;
+	prediction_t result = bp.predict(pc);
+	*dst = result.target;
+	return result.branch;
 }
 
 void BP_update(uint32_t pc, uint32_t targetPc, bool taken, uint32_t pred_dst){
-	return;
+	bp.update(pc, targetPc, taken, pred_dst);
 }
 
 void BP_GetStats(SIM_stats *curStats){
-	return;
+	*curStats = bp.getStats();
 }
 
